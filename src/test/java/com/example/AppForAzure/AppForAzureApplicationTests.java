756c6def6ab38bb008f232ce0f28a465872f8b5f
sgajendran <sgn1676@gmail.com>
2024-09-16 10:54:46 +0530

    Update AppForAzureApplicationTests.java

--- src/test/java/com/example/AppForAzure/AppForAzureApplicationTests.java
+++ src/test/java/com/example/AppForAzure/AppForAzureApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.AppForAzure;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AppForAzureApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
